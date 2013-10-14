c7fc61a63507b927b988f08e3020023a9df3a829
Sebastian Seilund <sebastian@seilund.com>
2013-10-13 22:17:21 -0700

    compass `importPaths` are now taken from `billy-builder.dependencyDirs`

--- tasks/compass.js
+++ tasks/compass.js
@@ -11,13 +11,17 @@ module.exports = function(grunt) {
     
     var versionPrefix = config.getVersionPrefix(grunt);
     
+    var importPaths = grunt.config.get('billy-builder.dependencyDirs').map(function(dir) {
+        return path.resolve(dir);
+    });
+    
     grunt.config.set('compass', {
         dist: {
             options: {
                 sassDir: grunt.config.get('billy-builder.compass.sassDir'),
                 cssDir: 'dist/'+versionPrefix+'css',
                 environment: process.env.NODE_ENV || 'development',
-                importPath: [path.resolve('./new-components')]
+                importPath: importPaths
             }
         }
     });
