3f14e02eac0a360ff23011270ff518818d9ccf14
Sebastian Seilund <sebastian@seilund.com>
2013-10-18 12:03:45 -0700

    Added user friendly error message if phantomjs is not installed

--- tasks/test-runner.js
+++ tasks/test-runner.js
@@ -8,6 +8,9 @@ module.exports = function(grunt) {
         var phantomjs = childProcess.spawn('phantomjs', [path.join(__dirname, '../phantom-test-runner.js')]);
         phantomjs.stdout.pipe(process.stdout);
         phantomjs.stderr.pipe(process.stderr);
+        phantomjs.on('error', function(err) {
+            grunt.fail.fatal('Could not run `phantomjs`: '+err+'\n\nPlease make sure that you have PhantomJS (http://phantomjs.org/) installed and that it\'s accessible in your PATH as `phantomjs`.');
+        });
         phantomjs.on('exit', function(code) {
             if (code !== 0) {
                 grunt.fail.fatal('Test runner exited with code '+code);
